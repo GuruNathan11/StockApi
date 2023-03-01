618a2101bad84aa3287b213031d783731d752a90
GuruNathan11 <121783817+GuruNathan11@users.noreply.github.com>
2023-03-01 16:31:38 +0530

    Update routes.js

--- Routes/routes.js
+++ Routes/routes.js
@@ -3,7 +3,7 @@ let router = require('express').Router();
 router.get('/',function(req,res){
     res.json({
         status : 'API Works',
-        message : "Welcome to Saving"
+        message : "Welcome to Stock-Market"
     })
 });
 
